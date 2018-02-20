895a27819d1afa9142a60a5932cb7cd621b7570f
Daniel Rużewicz <d.ruzew@gmail.com>
2018-02-20 18:47:29 +0100

    Added Address Controller that uses Entity Framework

---
+++ FormularzZDataBase/Controllers/AddressesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FormularzZDataBase.BusinessLogic;
+using FormularzZDataBase.Models;
+using FormularzZDataBase.Repository;
+
+namespace FormularzZDataBase.Controllers
+{
+    public class AddressesController : Controller
+    {
+
+        private readonly AddressRepository _addressRepository;
+
+        public AddressesController()
+        {
+                _addressRepository = new AddressRepository();
+        }
+
+        // GET: Addresses
+        public ActionResult Index()
+        {
+            return View(_addressRepository.GetWhere(x=>x.Id>0));
+        }
+
+        // GET: Addresses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Address address = _addressRepository.GetWhere(x=>x.Id == id).FirstOrDefault();
+            if (address == null)
+            {
+                return HttpNotFound();
+            }
+            return View(address);
+        }
+
+        // GET: Addresses/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Addresses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,PostCode,City,Street,BuildingNr")] Address address)
+        {
+            var validator = new PostCodeValidator();
+            var result = validator.Validate(address);
+            if (result.IsValid)
+            {
+                _addressRepository.Create(address);
+                
+                return RedirectToAction("Index");
+            }
+
+            ModelState.Clear();
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.ErrorMessage);
+            }
+
+            return View(address);
+        }
+
+        // GET: Addresses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Address address = _addressRepository.GetWhere(x=>x.Id== id).FirstOrDefault();
+            if (address == null)
+            {
+                return HttpNotFound();
+            }
+            return View(address);
+        }
+
+        // POST: Addresses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,PostCode,City,Street,BuildingNr")] Address address)
+        {
+            if (ModelState.IsValid)
+            {
+                _addressRepository.Update(address);
+               
+                return RedirectToAction("Index");
+            }
+            return View(address);
+        }
+
+        // GET: Addresses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Address address = _addressRepository.GetWhere(x => x.Id == id).FirstOrDefault();
+            if (address == null)
+            {
+                return HttpNotFound();
+            }
+            return View(address);
+        }
+
+        // POST: Addresses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Address address = _addressRepository.GetWhere(x => x.Id == id).FirstOrDefault();
+           _addressRepository.Delete(address);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
