09dd0c514133bc32f2975eae07266b3d26cc0a16
Daniel Rużewicz <d.ruzew@gmail.com>
2018-02-20 18:30:15 +0100

    Added 2 models: Address for address and "addressContext" for DataBase Context

---
+++ FormularzZDataBase/Models/AdressContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FormularzZDataBase.Models
+{
+    public class AdressContext : DbContext
+    {
+        public AdressContext() : base("Addressess")
+        {
+       
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
