4860320d64e173a636b9307dcb7eb875dc37c710
Daniel Rużewicz <d.ruzew@gmail.com>
2018-02-20 19:42:18 +0100

    Added 2 repositories: a)Abstract - with virtual methods that suit most cases b)AddressRepository that heritages from it and can override it for some special cases

---
+++ FormularzZDataBase/Repository/AddressRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FormularzZDataBase.Models;
+
+namespace FormularzZDataBase.Repository
+{
+    public class AddressRepository : AbstractRepository<Address>
+    {
+    }
+}
